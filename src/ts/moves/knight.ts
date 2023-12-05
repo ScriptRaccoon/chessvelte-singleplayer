913c763a796599b21f8b55e998667290ebfe7afa
Script Raccoon <scriptraccoon@gmail.com>
2023-12-05 01:06:31 +0100

    knight moves

---
+++ src/ts/moves/knight.ts
@@ -0,0 +1,30 @@
+import type { Piece, Coord } from "@/ts/types"
+import { is_valid } from "../utils"
+import type { Board } from "../Board"
+
+export function knight_moves(
+	piece: Piece,
+	coord: Coord,
+	board: Board
+): Coord[] {
+	const [row, col] = coord
+
+	const all_moves: Coord[] = [
+		[row + 1, col + 2],
+		[row - 1, col + 2],
+		[row + 2, col + 1],
+		[row - 2, col + 1],
+		[row + 1, col - 2],
+		[row - 1, col - 2],
+		[row + 2, col - 1],
+		[row - 2, col - 1],
+	]
+
+	const moves: Coord[] = all_moves.filter((_coord) => {
+		return is_valid(_coord) && board.get(_coord)?.color != piece.color
+	})
+
+	console.dir(moves)
+
+	return moves
+}
--- src/ts/moves/moves.ts
+++ src/ts/moves/moves.ts
@@ -1,6 +1,7 @@
 import type { Board } from "../Board"
 import { type Piece, type Coord } from "../types"
 import { bishop_moves } from "./bishop"
+import { knight_moves } from "./knight"
 
 import { pawn_moves } from "./pawns"
 import { queen_moves } from "./queen"
@@ -15,7 +16,7 @@ export function moves(piece: Piece, coord: Coord, board: Board): Coord[] {
 		case "bishop":
 			return bishop_moves(piece, coord, board)
 		case "knight":
-			return []
+			return knight_moves(piece, coord, board)
 		case "queen":
 			return queen_moves(piece, coord, board)
 		case "king":
